56bf7f881de46e0280f25b1c2ee5533db893a8df
CharlieRick <charick@me.com>
2021-04-19 12:07:03 +0100

    use data to present available information

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useState } from "react";
 import styled from "styled-components";
 import { SearchBar } from "./components/SearchBar";
 import "./styles/App.scss";
+import ExterImg from "./assets/Exeter.jpg";
+import ManchesterImg from "./assets/Manchester.jpg";
+import BirminghamImg from "./assets/Birmingham.jpg";
+import LondonImg from "./assets/London.jpg";
 
 const Container = styled.div`
   height: 100vh;
@@ -10,13 +14,53 @@ const Container = styled.div`
   justify-content: center;
   align-items: center;
   flex-direction: column;
+  background-image: "assets/Exeter.jpg";
+  background-size: cover;
+  background-position: center;
+`;
+
+const ExamplesContainer = styled.div`
+  margin-bottom: 48px;
+  margin-top: auto;
+  text-align: center;
+  background: rgba(0, 0, 0, 0.5);
+  padding: 24px 48px;
+  border-radius: 24px;
 `;
 
 const App = () => {
+  const [location, setLocation] = useState("");
+
+  const getBackground = () => {
+    switch (location) {
+      case "Exeter":
+        return ExterImg;
+      case "London":
+        return LondonImg;
+      case "Birmingham":
+        return BirminghamImg;
+      case "Manchester":
+        return ManchesterImg;
+    }
+  };
+
   return (
-    <Container>
-      <h1>Postcode Lookup</h1>
-      <SearchBar />
+    <Container style={{ backgroundImage: `url(${getBackground()})` }}>
+      <h1 style={{ marginTop: "auto", marginBottom: "48px" }}>
+        Postcode Lookup
+      </h1>
+      <SearchBar
+        handleUpdateData={(e: string) => {
+          setLocation(e);
+        }}
+      />
+      <ExamplesContainer>
+        <p>Example postcodes</p>
+        <p>E8 3DB</p>
+        <p>M5 3AA</p>
+        <p>B9 4BB</p>
+        <p>EX1 3AB</p>
+      </ExamplesContainer>
     </Container>
   );
 };
--- app/src/components/SearchBar/index.tsx
+++ app/src/components/SearchBar/index.tsx
@@ -1,5 +1,10 @@
-import React, { useEffect, useState } from "react";
+import React, { FunctionComponent, useEffect, useState } from "react";
 import styled from "styled-components";
+
+import CloudsIcon from "../../assets/CloudsIcon.svg";
+import RainIcon from "../../assets/RainIcon.svg";
+import ClearIcon from "../../assets/ClearIcon.svg";
+import SnowIcon from "../../assets/SnowIcon.svg";
 
 const Container = styled.div`
   height: 64px;
@@ -29,9 +34,27 @@ const DataContainer = styled.div`
   background: #fff;
   border-radius: 24px;
   display: flex;
+  justify-content: space-between;
   align-items: center;
   padding: 24px;
   color: #212121;
+  box-shadow: 2px 5px 16px 0px #000;
+
+  .area {
+    color: #d8d8d8;
+  }
+
+  .name {
+    font-size: 2rem;
+  }
+
+  .temp {
+    margin-right: 24px;
+    
+    .icon {
+        margin-left: 24px;
+    }
+  }
 `;
 
 interface ILocationData {
@@ -73,7 +96,11 @@ interface IWeatherData {
   name: string;
 }
 
-export const SearchBar = () => {
+interface ISearchBar {
+  handleUpdateData: (e: string) => void;
+}
+
+export const SearchBar: FunctionComponent<ISearchBar> = (props) => {
   const [value, setValue] = useState("");
   const [locationData, setLocationData] = useState<ILocationData | null>(null);
   const [weatherData, setWeatherData] = useState<IWeatherData | null>(null);
@@ -97,10 +124,24 @@ export const SearchBar = () => {
       ).then((res) => {
         res.json().then((response) => {
           setWeatherData(response[0]);
+          props.handleUpdateData(response[0].name);
         });
       });
     }
-  }, [locationData]);
+  }, [locationData, props.handleUpdateData]);
+
+  const getIcon = () => {
+    switch (weatherData?.weather[0].main) {
+      case "Clouds":
+        return CloudsIcon;
+      case "Rain":
+        return RainIcon;
+      case "Clear":
+        return ClearIcon;
+      case "Snow":
+        return SnowIcon;
+    }
+  };
 
   return (
     <>
@@ -115,7 +156,19 @@ export const SearchBar = () => {
 
       {locationData && (
         <DataContainer>
-          {locationData.nhs_ha} {weatherData?.name}
+          <div>
+            <div className="area">{locationData.nhs_ha}</div>
+            <div className="name">{weatherData?.name}</div>
+          </div>
+
+          <div className="temp">
+            {weatherData && (weatherData?.main.temp / 10).toFixed(0)}°C
+            <img
+              src={`url(${getIcon()})`}
+              alt={weatherData?.weather[0].main}
+              className="icon"
+            />
+          </div>
         </DataContainer>
       )}
     </>
